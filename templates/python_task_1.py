aa122725d88d06263870c2809923e28f5bd86728
Mansi Gupta <34365167+mansi32@users.noreply.github.com>
2023-12-10 13:44:24 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -14,6 +14,18 @@ def generate_car_matrix(df)->pd.DataFrame:
     """
     # Write your logic here
 
+    # Creating matrix(new_df) based on id_1, id_2, car values
+    df = df.pivot(index='id_1', columns='id_2', values='car')
+
+    # Fill NaN values with 0
+    df = df.fillna(0)
+
+    # Set diagonal values of new_df to 0
+    for i in df.index:
+        for j in df.columns:
+            if i == j:
+                df.loc[i, j] = 0
+
     return df
 
 
@@ -28,8 +40,18 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
+        
+    # Adding a new categorical column 'car_type' based on values of the 'car' column
+    df['car_type'] = pd.cut(df['car'], bins=[-float('inf'), 15, 25, float('inf')],
+                            labels=['low', 'medium', 'high'], right=False)
 
-    return dict()
+    # Calculate the count of occurrences for each 'car_type' category
+    counts = df['car_type'].value_counts().to_dict()
+
+    # Sort the dictionary alphabetically based on keys
+    counts = sorted(counts.items())
+    
+    return dict(counts)
 
 
 def get_bus_indexes(df)->list:
@@ -44,7 +66,16 @@ def get_bus_indexes(df)->list:
     """
     # Write your logic here
 
-    return list()
+    # Calculate the mean value of the 'bus' column
+    bus_mean = df['bus'].mean()
+
+    # Identify indices where the 'bus' values are greater than twice the mean
+    bus_indexes = df[df['bus'] > 2 * bus_mean].index.tolist()
+
+    # Sort the indices in ascending order
+    bus_indexes.sort()
+
+    return list(bus_indexes)
 
 
 def filter_routes(df)->list:
@@ -59,7 +90,16 @@ def filter_routes(df)->list:
     """
     # Write your logic here
 
-    return list()
+    # Calculating the average of values in the 'truck' column for each route
+    route_avg_truck = df.groupby('route')['truck'].mean()
+
+    # Filter routes where the average of 'truck' values is greater than 7
+    filtered_routes = route_avg_truck[route_avg_truck > 7].index.tolist()
+
+    # Sort the list of selected routes
+    filtered_routes.sort()
+
+    return list(filtered_routes)
 
 
 def multiply_matrix(matrix)->pd.DataFrame:
@@ -73,6 +113,12 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
+        
+    # Applying the specified logic to modify each value in the DataFrame
+    matrix = matrix.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25)
+
+    # Round the values to 1 decimal place
+    matrix = matrix.round(1)
 
     return matrix
 
